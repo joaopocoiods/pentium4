f0721460f73e54c60ae2a0f5346555a9afadcf68
pocoio21 <soares.oliveira21@escola.pr.gov.br>
2026-08-17 12:13:19 +0000

    aula3

--- experimento02.js
+++ experimento02.js
@@ -1,6 +1,7 @@
-onst botao02 document.getElementById ("botao02");
+const botao02 = document.getElementById("botao02");
 
 botao02.addEventListener("click", function(){
 
     botao02.style.backgroundColor = "red";
+    
 })
---
+++ experimento03.js
@@ -0,0 +1,6 @@
+const botao03 = document.getElementById("botao03");
+const paragrafo03 = document.getElementById("texto03");
+
+botao03.addEventListener("click", Function(){
+    paragrafo03textContet = "O texto mudou!"
+});
